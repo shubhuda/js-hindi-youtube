424c153d2f6181d645277c5c86bddad43e6a680a
Shubham Saxena <shubhsaxena2530@gmail.com>
2025-01-09 08:44:44 +0000

    committing introduction file for the series

---
+++ 01_basics/00_introduction.js
@@ -0,0 +1,4 @@
+// Earlier we had to create index.html file then write scrpt tag and link js file to run anythin in JS file
+// Now that software which was responible for interpreting and doing things after understanding JS file extracted out from the browser
+// We have Node js and Deno js a runtime environment for Javascript
+// Created by Ryan Dahl
